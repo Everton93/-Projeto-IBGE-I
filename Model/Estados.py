b2b841fc35d7755cb839a7999b86a00e6de57397
Everton93 <evesilvacosta@gmail.com>
2023-05-06 22:10:51 -0300

    Delete Model directory

--- Model/Estados.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Estados(object):                  
-                  
-    codigo_estado : int   
-    nome_uf : str 
-    sigla : str
-        
--- Model/Municipio.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from Model.Estados import Estados
-
-@dataclass
-class Municipio():
-        
-    nomeMunicipio : str
-    codigoMunicipio : int 
-    estado : Estados
